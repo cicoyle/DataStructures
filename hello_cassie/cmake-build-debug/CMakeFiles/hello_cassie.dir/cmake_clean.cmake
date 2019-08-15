cd128bed5b8c27e9f31650ad8210fd0cb9ca7f4e
cic26 <cic26@txstate.edu>
2019-08-15 11:09:00 -0500

    past hello world to make sure CLion worked

---
+++ hello_cassie/cmake-build-debug/CMakeFiles/hello_cassie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cassie.dir/main.cpp.obj"
+  "hello_cassie.pdb"
+  "hello_cassie.exe"
+  "hello_cassie.exe.manifest"
+  "libhello_cassie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cassie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello_cassie/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, cassie!" << std::endl;
+    return 0;
+}
